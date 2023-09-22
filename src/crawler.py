e48c92ad7a08e00ae2f912f25b82c387c9101b5f
Mohammad Mahdi Vahedi <m.m.vahedi13800@gmail.com>
2023-09-22 19:12:09 +0330

    Update Repository.py

--- src/crawler.py
+++ src/crawler.py
@@ -3,12 +3,14 @@ from bs4 import BeautifulSoup
 from queue import Queue, Empty
 from concurrent.futures import ThreadPoolExecutor
 from urllib.parse import urljoin, urlparse 
+from repository import Repository
+from commit import Commit
  
 class MultiThreadedCrawler:
 
     github_url = "https://github.com/"
 
-    user_agents = [
+    User_Agents = [
         "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1", 
         "Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0",
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10; rv:33.0) Gecko/20100101 Firefox/33.0",
@@ -17,42 +19,14 @@ class MultiThreadedCrawler:
         "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36"
     ]
 
-    def __init__(self, owner_name : str, repository_name : str, num_workers : int = 10):
-        self.owner_name = owner_name
-        self.repository_name = repository_name
-        self.repository_url = f'{self.github_url}{owner_name}/{repository_name}'
-        self.pool = ThreadPoolExecutor(max_workers=num_workers)
+    def __init__(self, repository : Repository, num_workers : int = 10):
+        self.repo = repository
+        self.pool = ThreadPoolExecutor(max_workers = num_workers)
         self.crawl_queue = Queue()
         self.session = requests.Session()
-        self.branch_name = self.find_branch_name()
-
-
-    def find_branch_name(self):
-        ripo_page = self.get_repo_page()
-        soup = BeautifulSoup(ripo_page, "xml")
-        branch_select_menu = soup.find_all(
-            name = 'details',
-            attrs = {
-                'id' : 'branch-select-menu'
-            }
-        )[0]
-        branch_name = branch_select_menu.find_all(
-            name = 'span',
-            attrs = {
-                'class' : 'css-truncate-target'
-            }
-        )[0].get_text()
-        return branch_name
-
-    def get_repo_page(self):
-        status_code, content = self.scrape_page(self.repository_url)
-        if status_code != 200:
-            # TODO: handle error
-            raise NotImplemented()
-        return content
 
     def scrape_page(self, url):
-        self.session.headers.update({'user-agent': random.sample(self.user_agents, 1)[0]})
+        self.session.headers.update({'user-agent': random.sample(self.User_Agents, 1)[0]})
         try:
             response = self.session.get(url)
             return response.status_code, response.text
@@ -81,16 +55,16 @@ class MultiThreadedCrawler:
                 continue
 
     def find_repo_commits_links(self):
-        starting_commit_page_url = self.repository_url + f'/commits/{self.branch_name}'
+        starting_commit_page_url = self.repo.get_URL() + f'/commits/{self.repo.branch}'
         status_code, content = self.scrape_page(starting_commit_page_url)
         if status_code != 200:
             # TODO: handle error
             raise NotImplemented()
         commit_list_page = BeautifulSoup(content, "xml")
         button = self.get_button(commit_list_page)
-        commits = self.get_commits_links(commit_list_page)
+        commits = self.parse_commits(commit_list_page)
 
-        while button.has_attr("disabled"):
+        while not button.has_attr("disabled"):
             status_code, content = self.scrape_page(starting_commit_page_url)
             commit_list_page = BeautifulSoup(content, "xml") 
             commits += self.get_commits_links(commit_list_page)
@@ -129,16 +103,8 @@ class MultiThreadedCrawler:
         }
         return datetime(*list(map(int,date.values())))
 
-    def parse_links(self, html):
-        soup = BeautifulSoup(html, 'html.parser')
-        Anchor_Tags = soup.find_all('a', href=True)
-        for link in Anchor_Tags:
-            url = link['href']
-            if url.startswith('/') or url.startswith(self.root_url):
-                url = urljoin(self.root_url, url)
-                if url not in self.scraped_pages:
-                    self.crawl_queue.put(url)
- 
+
+
     def scrape_info(self, html):
         soup = BeautifulSoup(html, "html5lib")
         web_page_paragraph_contents = soup('p')
@@ -165,38 +131,6 @@ class MultiThreadedCrawler:
  
  
 if __name__ == '__main__':
-    cc = MultiThreadedCrawler('mybatis','mybatis-3',1)
+    repo = Repository('mybatis','mybatis-3')
+    cc = MultiThreadedCrawler(repo, 1)
     print(cc.branch_name)
-    
-
-
-
-
-# import requests
-# from bs4 import BeautifulSoup
-
-# # class commit:
-# #     def __init__(self) -> None:
-# #         self.ripo = 
-# #         self.message = 
-# #         self.data = 
-# #         self.link = 
-# #         self.diff = 
-# #         pass
-
-# def find_commits_of_given_ripo(url):
-#     res = requests.get(url, timeout=(3, 30))
-#     soup = BeautifulSoup(res.text, "xml")
-#     timeline_items = soup.find_all("div", class_="TimelineItem-body")
-#     for item in timeline_items:
-#         month, day, year = item.find_all("h2")[0].get_text().replace(',', '').split()[-3:]
-#         commits = item.find_all("p", class_="mb-1")
-#         link_to_commits = list(map(lambda x: x.find('a', class_="Link--primary text-bold js-navigation-open markdown-title"), commits))
-    
-
-
-
-# url = "https://github.com/mybatis/mybatis-3/commits/master"
-# find_commits_of_given_ripo(url)
-
-# <a class="Link--primary text-bold js-navigation-open markdown-title" href="/ityouknow/spring-boot-examples/commit/8897031cb6026de6d3366a482564e1ddfd465765">Merge pull request</a>
--- src/repository.py
+++ src/repository.py
@@ -1,4 +1,6 @@
 import requests
+from bs4 import BeautifulSoup
+
 
 class Repository:
     All_Repositories = []
@@ -7,13 +9,35 @@ class Repository:
     def __init__(self, owner : str, name : str):
         self.owner = owner
         self.name = name
-        self.is_ripo_valid()
+        status_code, text = self.fetch_repo_page()
+        self.is_ripo_valid(status_code)
+        self.page = BeautifulSoup(text, "xml")
+        self.branch = self.get_branch_name()
         self.commits = []
         Repository.All_Repositories.append(self)
 
     def get_URL(self):
         return f'{Repository.Github_URL}/{self.owner}/{self.name}'
 
-    def is_ripo_valid(self):
-        if requests.get(self.get_url).status_code != 200:
+    def is_ripo_valid(self, status_code):
+        if status_code != 200:
             raise Exception('Invalid Repository!')
+        
+    def fetch_repo_page(self):
+        page = requests.get(self.get_url)
+        return page.status_code, page.text
+
+    def get_branch_name(self):
+        branch_select_menu = self.page.find_all(
+            name = 'details',
+            attrs = {
+                'id' : 'branch-select-menu'
+            }
+        )[0]
+        branch_name = branch_select_menu.find_all(
+            name = 'span',
+            attrs = {
+                'class' : 'css-truncate-target'
+            }
+        )[0].get_text()
+        return branch_name
